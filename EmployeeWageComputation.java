78e3930577462d16c6a65cc2be620c35fcc9dd87
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-10-10 15:50:20 +0530

    commiting Emp wage for multiple company using array

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,20 +1,20 @@
 package com.company1.EmployeeWageComputationUsingOOps;
 
+import java.util.Random;
+
 public class EmployeeWageComputation  {
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage Computation Program using OOP");
 
-        //creating object
-        EmployeeWageChildClass DMart = new EmployeeWageChildClass("D-Mart",20,10,50);
-        EmployeeWageChildClass StarHome = new EmployeeWageChildClass("Star Home",10,15,80);
 
-        //Getting Wages For Working Hour And Days for company
-        DMart.wagesForWorkingHourAndDays();
-        System.out.println(DMart);
-        StarHome.wagesForWorkingHourAndDays();
-        System.out.println(StarHome);
+        EmployeeWageChildClass employeeWage = new EmployeeWageChildClass();
 
+        employeeWage.addEmpWage("D-Mart",20,10,50);
+        employeeWage.addEmpWage("Star Home",10,15,80);
+        employeeWage.addEmpWage("infinity",25,20,80);
+        employeeWage.wagesForWorkingHourAndDays();
 
     }
 }
